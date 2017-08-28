62ea79a63f79aeffd3b792eafa6f702db95ccefa
habibsarbolandi <jsarbola@asu.edu>
2017-08-28 15:09:54 -0700

    Initial Commit

---
+++ hmwork1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hmwork1
+//
+//  Created by Habib Sarbolandi on 8/28/17.
+//  Copyright © 2017 Habib Sarbolandi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
